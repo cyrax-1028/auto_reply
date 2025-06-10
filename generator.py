b49c34472b5c8fe5144ec859ce6e5c32f0d8b810
Bahromjon <bahromjonsherqoziyev@gmail.com>
2025-06-10 19:29:11 +0500

    changed

--- generator.py
+++ generator.py
@@ -2,8 +2,8 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 # API ma'lumotlari
-API_ID = input("API ID kiriting: ")
-API_HASH = input("API Hash kiriting: ")
+API_ID = 27620089
+API_HASH = "5108a02b13715f247a66127db0f178ea"
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     session_string = client.session.save()
